1aacfc6dbc8a59b6979592acd857ee24538824ad
Nicole Miller <nicolemiller@Nicoles-MacBook-Air-2.local>
2021-03-10 00:13:55 -0500

    update to master

--- prediction.py
+++ prediction.py
@@ -13,7 +13,29 @@ def ticketSales(price, time, capacity, month, day, quarter):
 	inputs = np.array(jinputs);
   
   
- 	prediction = myModel.predict(polyTransf.transform(inputs)) 
+ 	prediction = myModel.predict(polyTransf.transform(inputs)) // predicts total sales
+
+ 	tSales = prediction / ticket_price
+	if(tSales > capacity):
+ 		prediction = capacity*ticket_price
+
+	if(prediction <= 0):
+		prediction = 0
+		tSales = 0
+
+	tSales_str = json.dumps(tSales);
+
+	return tSales_str
+
+def totalsales(price, time, capacity, month, day, quarter):
+	monthNum = {1: 0, 2: 31, 3: 59, 4: 90, 5: 120, 6: 151, 7: 181, 8: 212, 9:243, 10: 273, 11: 304, 12: 334 }
+ 	dayIndex = (monthNum[month] + day)
+	
+	jinputs = [(price, dayIndex, time, capacity, month, quarter, day)]
+	inputs = np.array(jinputs);
+  
+  
+ 	prediction = myModel.predict(polyTransf.transform(inputs)) // prediction holds total sales
 
  	tSales = prediction / ticket_price
 	if(tSales > capacity):
@@ -24,8 +46,5 @@ def ticketSales(price, time, capacity, month, day, quarter):
 		tSales = 0
 
 	pred_str = json.dumps(prediction);
-	tSales_str = json.dumps(tSales);
 
-	str = [pred_str, tSales_str]
-
-	return str
+	return pred_str
