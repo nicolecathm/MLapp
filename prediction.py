250641d41963596738e483c0a1db379b09ac50b3
Nicole Miller <nicolemiller@Nicoles-MacBook-Air-2.local>
2021-03-10 00:21:49 -0500

    update to master

--- prediction.py
+++ prediction.py
@@ -28,9 +28,10 @@ def ticketSales(price, time, capacity, month, day, quarter):
 	return tSales_str
 
 def totalsales(price, time, capacity, month, day, quarter):
-	monthNum = {1: 0, 2: 31, 3: 59, 4: 90, 5: 120, 6: 151, 7: 181, 8: 212, 9:243, 10: 273, 11: 304, 12: 334 }
+	monthNum = {1: 0, 2: 31, 3: 59, 4: 90, 5: 120, 6: 151, 7: 181, 8: 212, 9:243, 10: 273, 11: 304,12: 334 }
+
  	dayIndex = (monthNum[month] + day)
-	
+
 	jinputs = [(price, dayIndex, time, capacity, month, quarter, day)]
 	inputs = np.array(jinputs);
   
