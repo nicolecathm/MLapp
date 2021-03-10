b619e722099dfe5e4f8d4a1103a3b4df1888b864
Nicole Miller <nicolemiller@Nicoles-MacBook-Air-2.local>
2021-03-10 11:37:42 -0500

    insert parameters in master

--- prediction.py
+++ prediction.py
@@ -6,7 +6,7 @@ myModel = load('predTS.pkl')
 polyTransf = load('polyTransf.pkl')
 
 def ticketSales(price, time, capacity, month, day, quarter):
-	monthNum = {1: 0, 2: 31, 3: 59, 4: 90, 5: 120, 6: 151, 7: 181, 8: 212, 9:243, 10: 273, 11: 304, 12: 334 }
+	monthNum = {1:0,2:31,3:59,4:90,5:120,6:151,7:181,8:212,9:243,10:273,11:304,12:334}
  	dayIndex = (monthNum[month] + day)
 	
 	jinputs = [(price, dayIndex, time, capacity, month, quarter, day)]
@@ -28,7 +28,7 @@ def ticketSales(price, time, capacity, month, day, quarter):
 	return tSales_str
 
 def totalsales(price, time, capacity, month, day, quarter):
-	monthNum = {1: 0, 2: 31, 3: 59, 4: 90, 5: 120, 6: 151, 7: 181, 8: 212, 9:243, 10: 273, 11: 304,12: 334 }
+	monthNum = {1:0,2:31,3:59,4:90,5:120,6:151,7:181,8:212,9:243,10:273,11:304,12:334}
 
  	dayIndex = (monthNum[month] + day)
 
