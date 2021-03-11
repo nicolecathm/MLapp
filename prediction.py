ab14f12842a4c2602a467f4c77dc862986182e29
Nicole Miller <nicolemiller@Nicoles-MacBook-Air-2.local>
2021-03-11 15:46:00 -0500

    issue w ndarray fix

--- prediction.py
+++ prediction.py
@@ -24,7 +24,7 @@ def ticketSales(price, time, capacity, month, day, quarter):
 		
 	tSales_str = json.dumps(tSales);
 	
-	return tSales_str
+	return tSales
 
 def totalsales(price, time, capacity, month, day, quarter):
 	monthNum = {1:0,2:31,3:59,4:90,5:120,6:151,7:181,8:212,9:243,10:273,11:304,12:334}
